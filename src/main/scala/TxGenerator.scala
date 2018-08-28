41f0e9db1bc0035947d7b778dcd9def331fcc915
Stanislav Kapinus <kapinuss@rambler.ru>
2018-08-28 13:15:17 +0300

    second commit

--- src/main/scala/TxGenerator.scala
+++ src/main/scala/TxGenerator.scala
@@ -1,3 +1,12 @@
-object TxGenerator {
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
 
+import scala.concurrent.ExecutionContextExecutor
+
+object TxGenerator extends App {
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
+
+  system.log.info("hello")
 }
